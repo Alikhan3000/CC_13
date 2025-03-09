6d04b1218151cbba77d6f171560918ab6fc330ec
Alikhan <fixator2004@gmail.com>
2025-03-09 18:25:53 -0400

    Task 3 - Bulk Update on Employee Cards

--- cc_13.js
+++ cc_13.js
@@ -29,5 +29,26 @@ function buildEmployee(name, position){                 //created a function for
 }
 
 
+//test data:
+buildEmployee("Michael Kirkland", "Sales Associate");
+buildEmployee("Antonio Lightweight", "HR Director");
+
+
+// Task 3: Converting NodeLists to Arrays for Bulk Updates
+
+
+//creted a function with document.querySelectorAll that selects all employees with the employee card class
+function employeeSelect(){                          
+    const employeeQSA = document.querySelectorAll(".employeeid");
+
+    const employeeArray = [...employeeQSA];     //converted the NodeList into an array using the spread operator
+
+    employeeArray.forEach(employee => {         //used .forEach to apply backgroung and padding changes for the employee card
+        employee.style.background = "lightblue";
+        employee.style.padding = "1px";
+    });
+
+}
+employeeSelect(); //logged the function to see changes to the card
 
 
